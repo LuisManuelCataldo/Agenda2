a73cff54fe9dbe0acbcac3a05af9063bdd5e9548
Gonzalo Bechara <cacalo219@hotmail.com>
2022-09-05 09:33:32 -0300

    Fix Router: Nuevo módulo Home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
 const routes: Routes = [
   {
     path: "",
-    loadChildren: ()=> import('./app.component').then(m => m.AppComponent),
+    loadChildren: () => import('./public/pages/home/home.module').then(m => m.HomeModule)
   },
-  
-  { path: 'login',
-    loadChildren: ()=> import('./public/pages/login/login.module').then(m => m.LoginModule)
+  {
+    path: "login",
+    loadChildren: () => import('./public/pages/login/login.module').then(m => m.LoginModule)
   }
 ];
 
