93ae38a4527f3017ad840256bca8d22f8ccb5e48
Ayelen Villagra <ayelenvillagra@hotmail.com>
2022-09-13 19:45:32 -0300

    arreglo app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeRoutingModule } from './public/pages/home/home-routing.module';
+import { SignInComponent } from './public/pages/sign-in/sign-in.component';
 
 @NgModule({
   declarations: [
